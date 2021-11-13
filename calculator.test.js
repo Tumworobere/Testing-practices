c5bb0f565c3739d2bb6aaddc5fdcd9ec5d6e7e79
Tumworobere <annaht.annah@gmail.com>
2021-11-13 18:56:17 +0300

    Fix jest test errors

--- calculator.test.js
+++ calculator.test.js
@@ -2,28 +2,28 @@ const calc = require('./calculator.js')
 describe('calculator', () => {
 
   test('Add method', () => {
-    expect(calc.add(1, 3)).shouldBe(4);
-    expect(calc.add(2, 4)).shouldBe(6);
-    expect(calc.add(5, 6)).shouldBe(11);
+    expect(calc.add(1, 3)).toBe(4);
+    expect(calc.add(2, 4)).toBe(6);
+    expect(calc.add(5, 6)).toBe(11);
   });
 
   test('Subtract method', () => {
-    expect(calc.subtract(3, 1)).shouldBe(2);
-    expect(calc.subtract(4, 2)).shouldBe(2);
-    expect(calc.subtract(6, 5)).shouldBe(1);
+    expect(calc.subtract(3, 1)).toBe(2);
+    expect(calc.subtract(4, 2)).toBe(2);
+    expect(calc.subtract(6, 5)).toBe(1);
   });
 
 
   test('Divide method', () => {
-    expect(calc.divide(3, 1)).closerTo(2);
-    expect(calc.divide(5, 2)).closerTo(2.5);
-    expect(calc.divide(6, 3)).closerTo(2);
+    expect(calc.divide(3, 1)).toBeCloseTo(3);
+    expect(calc.divide(5, 2)).toBeCloseTo(2.5);
+    expect(calc.divide(6, 3)).toBeCloseTo(2);
   });
 
   test('Multiply method', () => {
-    expect(calc.multiply(3, 1)).shouldBe(3);
-    expect(calc.multiply(5, 3)).shouldBe(15);
-    expect(calc.multiply(4, 2.5)).shouldBe(10);
+    expect(calc.multiply(3, 1)).toBe(3);
+    expect(calc.multiply(5, 3)).toBe(15);
+    expect(calc.multiply(4, 2.5)).toBe(10);
   });
 
 
